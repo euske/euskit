26add05c2c44d80870746312c4916ec1a633e059
Yusuke Shinyama <yusuke@shinyama.jp>
2016-04-21 23:14:48 +0900

    Added more samples.

--- src/game.ts
+++ src/game.ts
@@ -6,19 +6,28 @@
 //   requires: text.ts
 //   requires: layer.ts
 //   requires: scene.ts
+//   requires: planmap.ts
+//   requires: planrunner.ts
 //   requires: app.ts
+
+function isObstacle(c:number) { return c == 1; }
+function isGrabbable(c:number) { return c == 2; }
+function isStoppable(c:number) { return c != 0; }
+PlatformerEntity.isObstacle = isObstacle;
+PlatformerEntity.isGrabbable = isGrabbable;
+PlatformerEntity.isStoppable = isStoppable;
 
 
 //  Player
 //
-class Player extends Entity {
+class Player extends PlatformerEntity {
 
     scene: Game;
     usermove: Vec2;
 
     constructor(scene: Game, pos: Vec2) {
 	let bounds = pos.expand(16, 16);
-	super(bounds, scene.sheet.get(0), bounds);
+	super(scene.tilemap, bounds, scene.sheet.get(0), bounds);
 	this.scene = scene;
 	this.usermove = new Vec2();
     }
@@ -38,23 +47,76 @@ class Player extends Entity {
 }
 
 
+//  Monster
+//
+class Monster extends PlanningEntity {
+
+    scene: Game;
+    target: Entity;
+
+    constructor(scene: Game, pos: Vec2) {
+	let bounds = pos.expand(16, 16);
+	super(scene.tilemap, bounds, scene.sheet.get(1), bounds);
+	this.scene = scene;
+    }
+
+    update() {
+	super.update();
+	if (!this.isPlanRunning()) {
+	    let runner = this.getPlan(this.target.hitbox.center())
+	    if (runner !== null) {
+		this.startPlan(runner);
+	    }
+	}
+	this.move();
+    }
+}
+
+
 //  Game
 // 
 class Game extends GameScene {
 
     player: Player;
+    tilemap: TileMap;
     sheet: SpriteSheet;
+    tiles: SpriteSheet;
 
     constructor(app: App) {
 	super(app);
-	this.sheet = new ImageSpriteSheet(app.images['sprites'], new Vec2(16,16));
+	this.sheet = new DummySpriteSheet(['green','red']);
+	this.tiles = new DummySpriteSheet(['black','gray','yellow']);
     }
     
     init() {
 	super.init();
+	this.tilemap = new TileMap(16, [
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+
+	    [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 1,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,1,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+	    [0,0,0,0, 0,0,1,0, 0,0,0,1, 1,0,0,0, 0,0,0,0],
+	    [0,0,1,1, 0,0,0,0, 1,1,0,0, 0,0,1,1, 0,0,0,0],
+	    
+	    [0,0,0,0, 0,0,1,0, 0,0,0,0, 0,1,0,0, 0,1,0,0],
+	    [0,0,0,0, 0,1,1,0, 0,0,0,0, 0,1,1,0, 0,0,0,1],
+	    [0,0,0,1, 0,0,0,0, 0,1,1,0, 0,1,1,1, 0,0,1,0],
+	    [0,0,1,1, 1,0,0,0, 0,0,0,0, 0,1,1,1, 1,0,0,0],
+	    [1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1],
+	]);
 	
 	this.player = new Player(this, this.screen.center());
 	this.addObject(this.player);
+	
+	PlanningEntity.initialize(16);
+	let monster = new Monster(this, this.screen.center())
+	monster.target = this.player;
+	this.addObject(monster);
 	
 	// show a banner.
 	let textbox = new TextBox(this.screen, this.app.font);
@@ -69,9 +131,16 @@ class Game extends GameScene {
 	this.player.setMove(this.app.key_dir);
     }
 
+    set_action(action: boolean) {
+	this.player.setJump(action? Infinity : 0);
+    }
+
     render(ctx: CanvasRenderingContext2D, bx: number, by: number) {
 	ctx.fillStyle = 'rgb(0,0,0)';
 	ctx.fillRect(bx, by, this.screen.width, this.screen.height);
+	this.tilemap.renderFromBottomLeft(
+	    ctx, bx, by, this.tiles,
+	    (x,y,c) => { return this.tilemap.get(x,y); });
 	super.render(ctx, bx, by);
     }
 }
