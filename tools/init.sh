0fa0aa93a042bbb7951e6cebf2e132f7109184be
Yusuke Shinyama <yusuke@shinyama.jp>
2016-04-30 07:32:44 +0900

    Added: init.sh

---
+++ tools/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# init.sh - project initialization script.
+
+# "strict mode"
+set -euo pipefail
+IFS=$'\n\t'
+
+# destdir.
+dst="$1"
+# dirname of the script.
+src=${0%/*}/..
+# basename of the destdir.
+name=${dst##*\/}
+[ -d "$dst" ] || ( echo "usage: $0 dst" && exit 1 )
+
+echo "src: $src"
+echo "dst: $dst"
+
+# create .rsyn
+cat > "$dst/.rsyn" <<EOF
+opts='--exclude *.wav --exclude *.md --exclude tmp --exclude tmp --exclude tools --exclude Makefile'
+remote='tabesugi:public/file/ludumdare.tabesugi.net/$name'
+EOF
+mkdir "$dst"/src || :  # ignore errors
+mkdir "$dst"/assets || :  # ignore errors
+cp "$src"/.gitignore "$dst"
+cp "$src"/Makefile "$dst"
+cp "$src"/src/*.ts "$dst"/src
+cp "$src"/assets/*.png "$dst"/assets
+cp "$src"/assets/*.mp3 "$dst"/assets
+cp "$src"/assets/Makefile "$dst"/assets
+sed -e "s/@@NAME@@/$name/g" "$src"/index.html > "$dst"/index.html
