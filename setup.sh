04d2be22c54b5b07cf233307d892461f168ed07d
Yusuke Shinyama <yusuke@shinyama.jp>
2016-07-27 15:10:18 +0900

    Moved: init.sh -> setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# setup.sh - project initialization script.
+
+# "strict mode"
+set -euo pipefail
+IFS=$'\n\t'
+
+# show the usage.
+[ 1 = $# ] || ( echo "usage: $0 destdir" && exit 111 )
+
+# destdir.
+dst="$1"
+# dirname of the script.
+src=${0%/*}
+mkdir "$dst" || :  # ignore errors
+[ -d "$dst" ] || ( echo "directory not exist: $dst" && exit 1 )
+
+echo "src: $src"
+echo "dst: $dst"
+
+# create .rsyn
+mkdir "$dst"/src || :  # ignore errors
+mkdir "$dst"/src/base || :  # ignore errors
+mkdir "$dst"/assets || :  # ignore errors
+cp "$src"/base/*.ts "$dst"/src/base
+cp "$src"/skel/.gitignore "$dst"
+cp "$src"/skel/Makefile "$dst"
+cp "$src"/skel/index.html "$dst"
+cp "$src"/skel/src/.gitignore "$dst"/src
+cp "$src"/skel/src/Makefile "$dst"/src
+cp "$src"/skel/src/*.ts "$dst"/src
+cp "$src"/skel/assets/.gitignore "$dst"/assets
+cp "$src"/skel/assets/* "$dst"/assets
